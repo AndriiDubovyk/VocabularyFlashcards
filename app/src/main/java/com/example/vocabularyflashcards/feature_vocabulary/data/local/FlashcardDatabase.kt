9891368278f6dbec9e1320b58ab221d7e8b66459
Andrii Dubovyk <and.dubovyk@gmail.com>
2022-06-05 15:09:35 +0300

    Add fixes for FlashcardDatabase class

--- app/src/main/java/com/example/vocabularyflashcards/feature_vocabulary/data/local/FlashcardDatabase.kt
+++ app/src/main/java/com/example/vocabularyflashcards/feature_vocabulary/data/local/FlashcardDatabase.kt
@@ -1,12 +1,17 @@
 package com.example.vocabularyflashcards.feature_vocabulary.data.local
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.example.vocabularyflashcards.feature_vocabulary.domain.model.Flashcard
 
 @Database(
     entities = [Flashcard::class],
     version = 1
 )
-abstract class FlashcardDatabase {
+abstract class FlashcardDatabase: RoomDatabase() {
     abstract val flashcardDao: FlashcardDao
+
+    companion object {
+        const val DATABASE_NAME = "flashcards_db"
+    }
 }
