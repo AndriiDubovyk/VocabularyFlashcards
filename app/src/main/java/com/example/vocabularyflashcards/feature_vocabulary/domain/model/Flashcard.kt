44ea4249bd1472636dc04178531cb10c2febc34f
Andrii Dubovyk <and.dubovyk@gmail.com>
2022-06-05 12:51:22 +0300

    Add annotations for Flashcard entity

--- app/src/main/java/com/example/vocabularyflashcards/feature_vocabulary/domain/model/Flashcard.kt
+++ app/src/main/java/com/example/vocabularyflashcards/feature_vocabulary/domain/model/Flashcard.kt
@@ -1,9 +1,13 @@
 package com.example.vocabularyflashcards.feature_vocabulary.domain.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class Flashcard(
     val word: String,
     val partOfSpeech: String,
     val translations: List<String>,
     val score: Int = 0, // represent user score for this word
-    val id: Int? = null
+    @PrimaryKey val id: Int? = null
 )
