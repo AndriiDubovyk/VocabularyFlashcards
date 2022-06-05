848912d5a6ca50123fb0b417776dbd338794efd8
Andrii Dubovyk <and.dubovyk@gmail.com>
2022-06-05 15:12:33 +0300

    Add FlashcardRepository providing

--- app/src/main/java/com/example/vocabularyflashcards/di/AppModule.kt
+++ app/src/main/java/com/example/vocabularyflashcards/di/AppModule.kt
@@ -3,6 +3,8 @@ package com.example.vocabularyflashcards.di
 import android.app.Application
 import androidx.room.Room
 import com.example.vocabularyflashcards.feature_vocabulary.data.local.FlashcardDatabase
+import com.example.vocabularyflashcards.feature_vocabulary.data.repository.FlashcardRepositoryImpl
+import com.example.vocabularyflashcards.feature_vocabulary.domain.repository.FlashcardRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -21,6 +23,12 @@ object AppModule {
             FlashcardDatabase::class.java,
             FlashcardDatabase.DATABASE_NAME
         ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideFlashcardRepository(db: FlashcardDatabase): FlashcardRepository {
+        return FlashcardRepositoryImpl(db.flashcardDao)
     }
 
 }
