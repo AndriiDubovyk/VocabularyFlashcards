ad1add2453e54cd4236b6ff67b43e4fc2efd6033
Andrii Dubovyk <and.dubovyk@gmail.com>
2022-06-06 17:34:28 +0300

    Add navigation processing to FlashcardScreen

--- app/src/main/java/com/example/vocabularyflashcards/feature_vocabulary/presentation/flashcard_list/FlashcardScreen.kt
+++ app/src/main/java/com/example/vocabularyflashcards/feature_vocabulary/presentation/flashcard_list/FlashcardScreen.kt
@@ -1,6 +1,8 @@
 package com.example.vocabularyflashcards.feature_vocabulary.presentation.flashcard_list
 
 
+import android.util.Log
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -9,15 +11,18 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.vocabularyflashcards.feature_vocabulary.presentation.flashcard_list.components.FlashcardItem
+import com.example.vocabularyflashcards.feature_vocabulary.presentation.util.UiEvent
 import com.example.vocabularyflashcards.ui.theme.Purple
 
 @Composable
 fun FlashcardScreen(
+    onNavigate: (UiEvent.Navigate) -> Unit,
     viewModel: FlashcardListViewModel = hiltViewModel()
 ) {
 
@@ -25,11 +30,24 @@ fun FlashcardScreen(
     val scaffoldState = rememberScaffoldState()
     val scope = rememberCoroutineScope()
 
+    LaunchedEffect(key1 = true) {
+        viewModel.uiEvent.collect { event ->
+            when (event) {
+                is UiEvent.Navigate -> {
+                    onNavigate(event)
+                }
+                else -> Unit
+            }
+        }
+    }
+
     Scaffold(
         scaffoldState = scaffoldState,
         floatingActionButton = {
             FloatingActionButton(
-                onClick = {/*TODO*/},
+                onClick = {
+                    viewModel.onAction(FlashcardListEvent.OnAddFlashcardClick)
+                },
                 backgroundColor = Purple
             ) {
                 Icon(imageVector = Icons.Default.Add, contentDescription = "Add flashcard")
@@ -45,7 +63,12 @@ fun FlashcardScreen(
             contentPadding = PaddingValues(start = 8.dp, end = 8.dp, top = 12.dp, bottom = 40.dp),
         ) {
             items(state.flashcards) { flashcard ->
-                FlashcardItem(flashcard = flashcard)
+                FlashcardItem(
+                    flashcard = flashcard,
+                    modifier = Modifier.clickable {
+                        viewModel.onAction(FlashcardListEvent.OnFlashcardClick(flashcard))
+                    }
+                )
             }
         }
     }
