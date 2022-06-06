3f25b0953c9cd4ca441796f7a057f289ff8254d2
Andrii Dubovyk <and.dubovyk@gmail.com>
2022-06-06 17:24:41 +0300

    Add Routes class for navigation

---
+++ app/src/main/java/com/example/vocabularyflashcards/feature_vocabulary/presentation/util/Routes.kt
@@ -0,0 +1,6 @@
+package com.example.vocabularyflashcards.feature_vocabulary.presentation.util
+
+object Routes {
+    const val FLASHCARD_LIST = "flashcard_list"
+    const val ADD_EDIT_FLASHCARD = "add_edit_flashcard"
+}
